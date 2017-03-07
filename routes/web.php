c973e7895763f2c86cc85b1f5dad81e5f8c6f9e9
Jack <mirumodepon@gmail.com>
2017-03-07 12:38:20 +0800

    EPISODE 8: Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Task;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,55 +11,5 @@ use App\Task;
 |
 */
 
-// Route::get('/', function () {
-//     // return view('welcome');
-//
-//     // 第一種
-//     // return view('welcome', [
-//     //   'name' => 'Laracasts',
-//     //   'age' => 3
-//     // ]);
-//
-//     // 第二種
-//     // return view('welcome')->with('name', 'World');
-//
-//     // 第三種
-//     // $name = 'Jeffrey';
-//     // return view('welcome', ['name' => $name]);
-//
-//     // 第四種
-//     $name = 'Jeffrey';
-//     $age = 31;
-//     // return view('welcome', compact('name', 'age'));
-//
-//     $tasks = [
-//       'Go to the store',
-//       'Finish my screencast',
-//       'Cleen the house'
-//     ];
-//
-//     return view('welcome', compact('name', 'age', 'tasks'));
-// });
-
-Route::get('/tasks', function () {
-    // $tasks = DB::table('tasks')->get();
-    // $tasks = DB::table('tasks')->where('created_at', '>=', '2017-3-3 08:00')->get();
-    // $tasks = DB::table('tasks')->latest()->get();
-
-    $tasks = Task::all();
-
-    // return $tasks; // JSON
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-    // dd($id); // The dd function dumps the given variables and ends execution of the script.
-
-    // $task = DB::table('tasks')->find($id);
-    // dd($task);
-
-    $task = Task::find($id);
-
-    // return view('tasks/show', compact('tasks'));
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
