4bea523f294dc8eb840c866e7543f5f1b997917e
Jack <mirumodepon@gmail.com>
2017-03-09 17:03:47 +0800

    EPISODE 13: Rendering Posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Post;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::latest()->get();
+
+        return view('posts.index', compact('posts'));
     }
-    public function show()
+    public function show(Post $post)
     {
-        return view('posts.show');
+        return view('posts.show', compact('post'));
     }
     public function create()
     {
