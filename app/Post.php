7bfaeb55e7032fee6c8de19501f877cf9b3ec96a
Jack <mirumodepon@gmail.com>
2017-03-10 18:42:54 +0800

    EPISODE 15: Eloquent Relationships and Comments

--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,10 @@
 
 namespace App;
 
-// use Illuminate\Database\Eloquent\Model;
-
 class Post extends Model
 {
-    // protected $fillable = ['title', 'body'];
-    // protected $guarded = [];
+    public function comments()
+    {
+      return $this->hasMany(Comment::class);
+    }
 }
