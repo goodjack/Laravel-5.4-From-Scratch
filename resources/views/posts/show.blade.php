38ec2b495655958fa0ecfbb02034c722bf40f65d
Jack <mirumodepon@gmail.com>
2017-03-13 15:29:18 +0800

    EPISODE 16: Add Comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,11 +15,31 @@
             <strong>
               {{ $comment->created_at->diffForHumans() }}: &nbsp;
             </strong>
-            
+
             {{ $comment->body }}
           </li>
         @endforeach
       </ul>
+    </div>
+
+    {{-- Add a comment --}}
+    <hr>
+
+    <div class="card">
+      <div class="card-block">
+        <form method="POST" action="/posts/{{ $post->id }}/comments">
+          {{ csrf_field() }}
+
+          <div class="form-group">
+            <textarea name="body" placeholder="Your comment here." class="form-control" required></textarea>
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary">Add Comment</button>
+          </div>
+        </form>
+
+        @include('layouts.errors')
+      </div>
     </div>
   </div>
 @endsection
